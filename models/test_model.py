3722676e69f7d1a53defb7b5342d6d3c442c1bbc
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-10-19 23:43:16 +0700

    add tests about CPU

--- models/test_model.py
+++ models/test_model.py
@@ -1,10 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
 
 class TestTypes(str, Enum):
-    HTTP_REQUES = "HTTP_REQUEST"
+    HTTP_REQUEST = "HTTP_REQUEST"
+    SYSTEM_CPU = "SYSTEM_CPU"
 
 class HTTP_Request(BaseModel):
     type: TestTypes
     url: str
-    timedelta_in_seconds: float
+    timedelta_in_seconds: float = Field(gt=0)
+
+class SystemCpu(BaseModel):
+    type: TestTypes
+    duration_seconds: int = Field(ge=1)
+    max_cpu_percents: int = Field(ge=1)
